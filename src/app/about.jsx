e467a99aa88d896df55275f949e623082a24f024
gerardodelascuevas <gerdlcp@gmail.com>
2023-07-21 11:16:00 -0600

    second commit

---
+++ src/app/about.jsx
@@ -0,0 +1,10 @@
+export default function About(){
+    return (
+        <div>
+              <p> Desarrollador FullStack en busqueda de nuevos retos. 
+                Apasionado de la informatica y la programación he realizado diversos proyectos 
+                academicos, de voluntariado y freelance con los stacks PERN, MERN y con Electron JS         
+              </p>
+        </div>
+    )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,15 @@
+import About from "./about";
+import Navbar from "./navbar";
+
 export default function Home(){
   return(
-    <p> Portafolio de Gerardo de las Cuevas </p>
+    <div class="container mx-auto px-4"> 
+      <Navbar />
+      <h1 className="text-5xl text-center"> Hola Soy Gerardo, desarrollador Full Stack JavaScript </h1>
+      <About />
+     
+
+    </div>
+    
   )
 }
