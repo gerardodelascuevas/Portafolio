91e9b027ff5a9f083d9d66c8d29be8c4a8812184
gerardodelascuevas <gerdlcp@gmail.com>
2024-03-21 18:42:29 -0600

    cambiando presentacion en proyectos

--- src/app/about.jsx
+++ src/app/about.jsx
@@ -5,44 +5,8 @@ import ProfessionsDisplay from "./professions";
 
 export default function About() {
 
-  // const TypewriterEffect = () => {
-  //   const professions = ['Desarrollador Full Stack', 'Maestro en Estrategia Agroempresarial'];
-  //   const [index, setIndex] = useState(0); // Para controlar el índice de la profesión actual
-  //   const [subIndex, setSubIndex] = useState(0); // Para controlar el índice del carácter actual en la profesión
-  //   const [reverse, setReverse] = useState(false); // Para saber cuándo agregar o quitar caracteres
-  //   const [text, setText] = useState(''); // El texto actual a mostrar
-  //   const [speed, setSpeed] = useState(150); // Velocidad de escritura
-  // console.log(text)
-  //   useEffect(() => {
-  //     //COMENZAR DE NUEVO
-  //     if (index === professions.length) setIndex(0);
-  //     //COMENZAR A BORRAR
-  //     if (subIndex === professions[index].length + 1 && !reverse) {
-  //       setReverse(true);
-  //       setSpeed(100); 
-  //       //COMENZAR A ESCRIBIR
-  //     } else if (subIndex === 0 && reverse) {
-  //       setReverse(false);
-  //       setText(' ')
-  //       setIndex((prev) => (prev + 1) % professions.length);
-  //       setSpeed(150); // Velocidad de escritura
-  //     }
+  const professions = ['Desarrollador Full Stack', 'Ingeniero en Sistemas Computacionales', 'Administrador de Empresas', 'Maestro en Estrategia Agroempresarial'];
   
-  //     const timeout = setTimeout(() => {
-  //       setText((prev) => {
-  //         if (prev === undefined) return ' '; // Esto deja el estado sin cambios si prev es undefined
-  //         return reverse ? prev.slice(0, prev.length - 1) : prev + professions[index][subIndex];
-  //       });
-        
-  //       setSubIndex((prev) => (reverse ? prev - 1 : prev + 1));
-  //     }, speed);
-  
-  //     return () => clearTimeout(timeout);
-  //   }, [subIndex, index, reverse, speed]);
-  
-  //   return <h2 className="text-2xl text-center">{text}</h2>;
-  // };    
-  const professions = ['Desarrollador Full Stack', 'Maestro en Estrategia Agroempresarial'];
   return (
     <div className="mb-10 mt-20 " id="about">
       <h1 className="text-3xl text-center">Hola Soy Gerardo</h1>
--- src/app/projects.jsx
+++ src/app/projects.jsx
@@ -1,55 +1,67 @@
+'use client'
+
+import { useState } from "react";
+
 export default function Projects() {
+  
+  const proyectos = [
+    {
+      nombre: 'CattleMarket',
+      imagen: '/img/cattlemarketimage.png',
+      descripcion: 'Software de administración de hatos ganaderos que permite la medición de los parametros productivos del rancho asi como tambien brinda trazabilidad al ganado, con rutas públicas para venta de animales y privadas para su administración',
+      link: 'https://cattle-market.vercel.app',
+    },
+    {
+      nombre: 'SportsMarket',
+      imagen: '/img/sportsMarket.jpg',
+      descripcion: 'Tienda virtual que cuenta con panel de catálogo de productos, panel de administración, ventana de favoritos, entre otras funcionalidades',
+      link: 'https://github.com/e-commercePF/Client',
+    },   
+  ];
+
+  const [indiceActual, setIndiceActual] = useState(0);
+
+  const proyectoActual = proyectos[indiceActual];
+
+  const siguienteProyecto = () => {
+    setIndiceActual((indiceActual + 1) % proyectos.length); // Cicla al primer proyecto después del último
+  };
+
+  const proyectoAnterior = () => {
+    setIndiceActual((indiceActual - 1 + proyectos.length) % proyectos.length); // Va al último proyecto si está en el primero
+  };
+
   return (
     <div className="text-center" id='projects'>
       <h1 className="text-3xl p-4">Proyectos</h1>
   
-      <div className="flex flex-wrap justify-center gap-4">
-        {/* Proyectos Web */}        
-        <div>
-          <h2 className="text-center">Proyectos Web</h2>
-
-          {/* SPORTS MARKET */}
-          <div className="flex flex-wrap justify-center gap-4">
-            <div className="max-w-xs rounded overflow-hidden shadow-lg transition-all duration-300 hover:scale-110 m-4">
-              <img className="w-full h-64" src="/img/sportsMarket.jpg" alt="Sports Market" />
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">SportsMarket</div>
-                <p className="text-gray-700 text-base">
-                  Tienda virtual que cuenta con panel de catálogo de productos, panel de administración, ventana de favoritos
-                </p>
-              </div>
-              <div className="px-6 pt-4 pb-2">
-                {/* Tecnologías */}
-              </div>
-              <a href="https://github.com/e-commercePF/Client" className="bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded">
-                Ver el Repositorio
-              </a>
-            </div>
+      <div className="flex flex-wrap justify-center gap-4">     
+        
+         {/* Proyectos Web */} 
+         <div className="flex flex-col items-center">
+      <h1 className="text-xl text-center mb-4">Proyectos Web</h1>
+      <div className="relative ">
+        <div className="max-w-xs rounded overflow-hidden shadow-lg transition-all duration-300 hover:scale-110 m-4">
+          <img className="w-full h-64" src={proyectoActual.imagen} alt={proyectoActual.nombre} />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{proyectoActual.nombre}</div>
+            <p className="text-gray-700 text-base">{proyectoActual.descripcion}</p>
           </div>
-
-          
-          {/* SPORTS MARKET */}
-          <div className="flex flex-wrap justify-center gap-4">
-            <div className="max-w-xs rounded overflow-hidden shadow-lg transition-all duration-300 hover:scale-110 m-4">
-              <img className="w-full h-64" src="/img/cattlemarketimage.png" alt="Cattle Market" />
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">CattleMarket</div>
-                <p className="text-gray-700 text-base">
-                  Software de administración de hatos ganaderos que permite la medición de los parametros 
-                  productivos del rancho asi como tambien brinda trazabilidad al ganado, con rutas públicas 
-                  para venta de animales y privadas para su administración
-                </p>
-              </div>
-              <div className="px-6 pt-4 pb-2">
-                {/* Tecnologías */}
-              </div>
-              <a href="https://cattle-market.vercel.app" className="bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded">
-                Ver el Proyecto
-              </a>
-            </div>
-          </div>
+          <a href={proyectoActual.link} className="bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded mb-4 block text-center"
+          target="_blank"
+          >
+            Ver el Proyecto
+          </a>
         </div>
-  
+        <button onClick={proyectoAnterior} className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full">
+          &lt;
+        </button>
+        <button onClick={siguienteProyecto} className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full">
+          &gt;
+        </button>
+      </div>
+    </div>
+        
         {/* Proyectos Desktop */}
         <div>
           <h2 className="text-center">Proyectos Desktop</h2>
@@ -70,7 +82,11 @@ export default function Projects() {
               <div className="px-6 pt-4 pb-2">
                 {/* Tecnologías */}
               </div>
-              <a href="https://github.com/gerardodelascuevas/businesscontrolpos" className="bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded">
+              <a href="https://github.com/gerardodelascuevas/businesscontrolpos" 
+              className="bg-blue-500 hover:bg-blue-700 
+              cursor-pointer text-white font-bold py-2 px-4 rounded"
+              target="_blank"
+              >
                 Ver el Repositorio
               </a>
             </div>
