608476e8c23581e71803b4df71f0d9b9979d936e
gerardodelascuevas <gerdlcp@gmail.com>
2024-04-07 18:23:50 -0600

    update scroll behavior

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Gerardo De Las Cuevas Peña, Full Stack Developer',
   description: 'Powered by gdlcp',
-  keywords: 'javascript, developer, web developer, nodejs, react, express, portafolio'
+  keywords: 'javascript, developer, web developer, nodejs, react, express, PHP, Laravel, portafolio'
 }
 
 export default function RootLayout({ children }) {
