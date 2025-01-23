bdb84cda0f32d634d47c5d5207ec9fca8aec7cce
gerardodelascuevas <gerdlcp@gmail.com>
2025-01-22 20:32:14 -0600

    add technologies

--- src/app/techSkills.jsx
+++ src/app/techSkills.jsx
@@ -11,6 +11,8 @@ export default function TechSkills(){
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="Bootstrap"  src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/bootstrap/bootstrap-original.svg"  />
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="Tailwind"  src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/tailwindcss/tailwindcss-plain.svg"  />
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="JavaScript"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"  />
+
+                <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="TypeScript"  src="https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/typescript/typescript-original.svg"  />
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="React"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"  />
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="Node.js"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="MySQL"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"  />
@@ -21,6 +23,9 @@ export default function TechSkills(){
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="Git"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"  />
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="PHP"  src="https://raw.githubusercontent.com/devicons/devicon/v2.16.0/icons/php/php-original.svg"  /> 
                 <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="Laravel"  src="https://raw.githubusercontent.com/devicons/devicon/v2.16.0/icons/laravel/laravel-original.svg"  /> 
+                <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="Java"  src="https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/java/java-original.svg"  /> 
+                
+                <img className="max-w-sm transition-all duration-300 cursor-pointer group-hover:grayscale-0 w-12 m-4 hover:scale-150 w-12 m-4" alt="SpringBoot"  src="https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/spring/spring-original.svg"  /> 
 
             </div>
         </div>
